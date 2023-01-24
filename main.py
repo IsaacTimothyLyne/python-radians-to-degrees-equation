6969aeece66525b88fb84315c30b1574a1f3c4ed
Isaac Lyne <IsaactimothyLyne@gmail.com>
2023-01-25 11:07:44 +1300

    cxomment my hate

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import math
 radians = float(input('Enter radians to convert to degrees: '))
 result = math.radians(radians)
+# or
+# result = (radians*180)/math.pi 
+# although according to online the above equation should be correct, 1 radian does not equal pi in this equation
 print(f'{radians} Radians in degrees is {result}')
